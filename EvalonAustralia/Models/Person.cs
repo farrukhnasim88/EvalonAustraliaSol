4412daf06e39c7b49055da8598a554643237d7fc
FARRUKH Nasim <farrukh.nasim1@studytafensw.edu.au>
2020-05-26 19:59:41 +1000

    Add project files.

---
+++ EvalonAustralia/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvalonAustralia.Models
+{
+    public class Person
+    {
+        
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public IEnumerable<Vehicles> Vehicles { get; set; }
+
+
+
+
+    }
+}
---
+++ EvalonAustralia/Models/Vehicles.cs
@@ -0,0 +1,16 @@
+﻿namespace EvalonAustralia.Models
+{
+    public class Vehicles
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Km { get; set; }
+
+
+
+
+
+    }
+}
